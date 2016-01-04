2ef9f40857ce9617f04c7d527e50e20baad9c804
Nick Wilson <Nick Wilson>
2016-01-04 17:35:30 -0600

    finished FizzBuzz

--- JavaScriptFizzBuzz.js
+++ JavaScriptFizzBuzz.js
@@ -1,3 +1,18 @@
 "use strict";
+var counter;
+var item;
 
-for(counter=0, counter<=100, counter++);
+
+for(counter=0; counter<=100; counter++){
+console.log(counter)
+
+if (counter % 3 === 0){
+console.log("fizz") 
+if (counter % 5 === 0){
+console.log("buzz")
+if (counter % 15 === 0){
+console.log("fizzbuzz")
+}
+}
+}
+}
